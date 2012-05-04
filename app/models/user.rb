31f9b54ce0d6d7c74aa34d8b9835c6f7fca6d125
Robert Karlsson <RWKarlsson@yahoo.com>
2012-05-04 08:06:49 -0600

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :password, :password_confirmation, :password_digest
+
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+end 
